5de8a54b7b55c1d8d730bd541c7fbe884d112001
paulo-win <xtbr295@gmail.com>
2024-07-11 13:55:09 -0300

    re-styled loginpage

--- src/Componentes/Footer/FooterStyled.jsx
+++ src/Componentes/Footer/FooterStyled.jsx
@@ -1,47 +1,11 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
-  background-color: #252630;
+  background-color: #EF5E22;
   color: white;
-  padding: 40px 20px;
+  padding: 2%;
   text-align: center;
-`;
-
-export const FooterContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: space-around;
-  }
-`;
-
-export const FooterSection = styled.div`
-  margin: 20px 0;
-
-  p {
-    font-size: 0.9em;
-    line-height: 1.6;
-  }
-`;
-
-export const FooterTitle = styled.h4`
-  margin-bottom: 10px;
-  font-size: 1.2em;
-  font-weight: bold;
-`;
-
-export const FooterLink = styled.a`
-  color: #ffffff;
-  text-decoration: none;
-  margin-bottom: 10px;
-  display: block;
-
-  &:hover {
-    text-decoration: underline;
-    color: #007BFF;
-  }
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 18px;
 `;
--- src/Componentes/LoginPage/LoginPageComponent.jsx
+++ src/Componentes/LoginPage/LoginPageComponent.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import './LoginPageStyled.css'; // Certifique-se de criar um arquivo CSS para os estilos
-
+import './LoginPageStyled.css'; 
+import '../Footer/Footer'
 const Login = () => {
   return (
-    <div className="login-container">
-      <div className="login-logo">
-        <img src="./public/LOGO.svg" alt="CyberOS Logo" />
-      </div>
-      <div className="login-form">
-        <h1 className="titulo">Já tem cadastro?</h1>
-        <h2 className="subtitulo">Faça seu login__</h2>
-        <button className="google-login">
-          <img src="./public/google.png" alt="Google Logo" /> Login com Google
-        </button>
-        <div className="or-separator">- ou -</div>
-        <form>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
-        </form>
-        <div className="login-links">
-          <a href="/forgot-password">esqueceu sua senha?</a>
-          <a href="/register">Novo no cyberos? Cadastre-se aqui</a>
+    <section className='login-form'>
+      <div className="login-container">
+        <img className='logo-login' src="./public/LOGO.svg" alt="CyberOS Logo" />
+        <div className="login-form">
+          <h1 className="titulo">Já tem cadastro?</h1>
+          <h2 className="subtitulo">Faça seu login__</h2>
+          <button className="google-login">
+            <img src="./public/google.png" alt="Google Logo" /> Login com Google
+          </button>
+          <div className="or-separator">- ou -</div>
+          <form>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Senha" />
+            <button type="submit">Entrar</button>
+          </form>
+          <div className="login-links">
+            <a href="/forgot-password">esqueceu sua senha?</a>
+            <a href="/register">Novo no cyberos? Cadastre-se aqui</a>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
