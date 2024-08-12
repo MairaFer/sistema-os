afeb7f9f762e87cedf8f5552a8f3e3822c7a0ced
MairaFer <fernanda.maira2004@gmail.com>
2024-08-11 21:21:50 -0300

    seettingspage att

--- src/pages/Settings/SettingsPages.jsx
+++ src/pages/Settings/SettingsPages.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Container, Title, Form, Input, Button, Section } from './SettingsPageStyled';
+import { Container, FormContainer, Title, Form, Input, Button, Section, LogoContainer, LogoImage } from './SettingsPageStyled';
 
 const SettingsPage = () => {
   const [formData, setFormData] = useState({
     username: 'Seu Nome',
     email: 'seuemail@example.com',
-    password: ''
+    cnpjcpf: '12345678901',
+    phone: '1234567890',
   });
 
   const handleChange = (e) => {
@@ -15,56 +16,68 @@ const SettingsPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Lógica para enviar os dados do formulário
     console.log('Dados enviados:', formData);
+  };
+
+  const handleLogoChange = (e) => {
+    console.log('Logo alterada');
   };
 
   return (
     <Container>
-      <Title>Configurações da Conta</Title>
-      <Form onSubmit={handleSubmit}>
-        <Section>
-          <label htmlFor="username">Nome da empresa</label>
-          <Input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </Section>
-        <Section>
-          <label htmlFor="email">Email</label>
-          <Input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </Section>
-        <Section>
-          <label htmlFor="phone">Telefone</label>
-          <Input
-            type="text"
-            id="phone"
-            name="phone"
-            value={formData.telefone}
-            onChange={handleChange}
-          />
-        </Section>
-        <Section>
-          <label htmlFor="password">Nova Senha</label>
-          <Input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </Section>
-        <Button type="submit">Salvar Alterações</Button>
-      </Form>
+      <FormContainer>
+        <Title>Configurações da Conta</Title>
+        <Form onSubmit={handleSubmit}>
+          <Section>
+            <label htmlFor="username">Nome da empresa</label>
+            <Input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </Section>
+          <Section>
+            <label htmlFor="email">Email</label>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </Section>
+          <Section>
+            <label htmlFor="phone">Telefone</label>
+            <Input
+              type="text"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </Section>
+          <Section>
+            <label htmlFor="cnpjcpf">CNPJ/CPF</label>
+            <Input
+              type="text"
+              id="cnpjcpf"
+              name="cnpjcpf"
+              value={formData.cnpjcpf}
+              onChange={handleChange}
+            />
+          </Section>
+          <div style={{ display: 'flex', gap: '1rem' }}>
+            <Button type="button">Alterar Senha</Button>
+            <Button type="button" style={{ backgroundColor: '#dc3545' }}>Excluir Conta</Button>
+          </div>
+        </Form>
+      </FormContainer>
+      <LogoContainer>
+        <LogoImage src="/logoex.png" alt="Logo da Empresa" onClick={handleLogoChange} />
+        <p>Alterar foto da logo</p>
+      </LogoContainer>
     </Container>
   );
 };
